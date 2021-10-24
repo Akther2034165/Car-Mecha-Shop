4d0cb461314e99d38d4e1918d4e86a3f7808ac16
Akther Hosenn <nirjhorn457@gmail.com>
2021-10-24 18:46:21 +0600

    backend added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import AuthProvider from "./context/AuthProvider";
+import AddService from "./Pages/AddService/AddService";
 import Booking from "./Pages/Booking/Booking/Booking";
 import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login/Login";
 import PrivateRoute from "./Pages/Login/PrivateRoute/PrivateRoute";
+import ManageServices from "./Pages/ManageServices/ManageServices";
 import NotFound from "./Pages/NotFound/NotFound";
 import Header from "./Pages/Shared/Header/Header";
 
@@ -27,6 +29,12 @@ function App() {
             <PrivateRoute path="/booking/:serviceId">
               <Booking />
             </PrivateRoute>
+            <Route path="/addservice">
+              <AddService />
+            </Route>
+            <Route path="/manageservices">
+              <ManageServices />
+            </Route>
             <Route exact path="*">
               <NotFound />
             </Route>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Service.css";
 const Service = (props) => {
-  const { id, name, price, time, img, description } = props.service;
+  const { _id, name, price, time, img, description } = props.service;
   return (
     <div className="col-lg-4 col-md-6 col-12 mt-2 service">
       <img src={img} className="img-fluid" alt="" />
@@ -10,10 +10,8 @@ const Service = (props) => {
       <h5>{price}</h5>
       <h5>{time}</h5>
       <p className="px-3">{description}</p>
-      <Link to={`/booking/${id}`}>
-        <button className="btn btn-warning mb-3">
-          Book {name.toLowerCase()}
-        </button>
+      <Link to={`/booking/${_id}`}>
+        <button className="btn btn-warning mb-3">Book {name}</button>
       </Link>
     </div>
   );
